cd8d47b19c0ea691f968552e24578423b8b08d95
shivam779823 <105196334+shivam779823@users.noreply.github.com>
2022-10-03 13:45:39 +0530

    Add files via upload

---
+++ gcp-bastion-vm.tf
@@ -0,0 +1,149 @@
+provider "google" {
+  project = var.projectid
+  region = var.region
+  zone=var.zone
+}
+
+
+#VPC custom
+
+resource "google_compute_network" "vpc" {
+  name = var.vpc_name
+  auto_create_subnetworks =  true 
+  
+}
+
+
+resource "google_compute_subnetwork" "subnet" {
+    name = "subnet"
+    network = google_compute_network.vpc.name
+    ip_cidr_range = "0.0.0.0/0"
+    private_ip_google_access = true
+    region = var.region
+  
+}
+
+#firewall
+
+#allow-ssh-bastion
+
+resource "google_compute_firewall" "rule1" {
+  project     = var.projectid
+  name        = "allow-ssh-bastion"
+  network     = google_compute_subnetwork.subnet.name
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+  source_ranges = [ "35.235.240.0/20" ]
+  
+  target_tags = ["bastion"]
+}
+
+#allow-http-ingress
+
+resource "google_compute_firewall" "rule2" {
+  project     = var.projectid
+  name        = ""
+  network     = google_compute_subnetwork.subnet.name
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80"]
+  }
+  source_ranges = [ "0.0.0.0/0" ]
+  
+  target_tags = ["flipkart"]
+}
+
+#allow-ssh-bastion-flipkart
+
+
+resource "google_compute_firewall" "rule3" {
+  project     = var.projectid
+  name        = "allow-ssh-bastion-flipkart"
+  network     = google_compute_subnetwork.subnet.name
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+  source_ranges = [ "192.168.10.0/24" ]
+  
+  target_tags = ["flipkart" , "bastion"]
+}
+
+
+#bastion vm
+
+
+resource "google_compute_instance" "bastion" {
+  name         = "bastion"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["bastion"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = google_compute_subnetwork.subnet.name
+
+    
+  }
+
+ 
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+ 
+}
+
+
+#flipkatvm
+
+
+resource "google_compute_instance" "flipkart" {
+  name         = "flipkart"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["flipkart"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = google_compute_subnetwork.subnet.name
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+ 
+}
---
+++ varibales.tf
@@ -0,0 +1,18 @@
+variable "projectid" {
+  default= "proj"
+
+}
+
+variable "region" {
+  default= "us-central1"
+
+}
+variable "zone" {
+  default= "us-central1-a"
+
+}
+
+variable "vpc_name" {
+    default = "flipkartvpc"
+  
+}
